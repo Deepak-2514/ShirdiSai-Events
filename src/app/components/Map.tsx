c60edf54a8da980d306f3ad706fd70a5ce7b3705
Deepak-2514 <kjdeepak2514@gmail.com>
2026-06-09 20:48:33 +0300

    map update...

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -18,6 +18,3 @@ const Map = () => {
 };
 
 export default Map;
-{
-  /* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3848.8792564670985!2d76.3926668!3d15.274379100000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb9d56b19ab3f55%3A0xfd05d842b480a4de!2sShiridi%20Sai%20Events!5e0!3m2!1sen!2sin!4v1780849619015!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */
-}
